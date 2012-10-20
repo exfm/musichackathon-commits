2e3f9763acf350202a425734c4d2f3ae557fa0fc
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-20 18:15:26 -0400

    Oop should be span8

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ var socket = io.connect('http://musichackathon-commits.ex.fm');
 
 socket.on('commit', function (data) {
     console.log('Got commit!', data);
-    var h = '<div class="commit row" id="commit-'+data.id+'"><div class="image span2"><img src="'+data.image+'"/></div><div class="span10"><div class="header">';
+    var h = '<div class="commit row" id="commit-'+data.id+'"><div class="image span2"><img src="'+data.image+'"/></div><div class="span8"><div class="header">';
     h += data.username;
     h += '<span>&nbsp;pushed to&nbsp;</span>'+data.repoName;
     h += '</div><div class="message">'+data.message+'</div></div></div>';
