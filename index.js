9a5d204c088f9fa50aa3b7e0d19bd165fee5ca3c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-20 17:44:59 -0400

    Cleaned out old commits data

--- index.js
+++ index.js
@@ -235,7 +235,6 @@ io.sockets.on('connection', function(socket){
 server.listen(12000);
 
 
-
 bouncy(function (req, bounce) {
     console.log('Request', req);
     if(req.url === '/gith' && req.method === 'POST'){
