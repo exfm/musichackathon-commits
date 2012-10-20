260114f666907a9aa13e9a8d098770759ee0851f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-20 17:21:44 -0400

    nother shot..

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ var socket = io.connect('http://musichackathon-commits.ex.fm');
 socket.on('commit', function (data) {
     console.log('Got commit!', data);
 
+
     var h = '<div class="commit" id="commit-'+data.id+'" style="display: none;"><div class="header">';
     h += '<a href="https://github.com/'+data.username+'">'+data.username+'</a>';
     h += '<span>pushed to</span><a href="/repo/'+data.repoName+'">'+data.repoName+'</a>';
