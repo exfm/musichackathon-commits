2fd6bcc60ec2aa2382a1e2484543073d7d7a2ecb
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-20 17:21:03 -0400

    fucker

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ socket.on('commit', function (data) {
     var h = '<div class="commit" id="commit-'+data.id+'" style="display: none;"><div class="header">';
     h += '<a href="https://github.com/'+data.username+'">'+data.username+'</a>';
     h += '<span>pushed to</span><a href="/repo/'+data.repoName+'">'+data.repoName+'</a>';
-    h =+ '</div><div class="message">'+data.message+'</div></div>';
+    h += '</div><div class="message">'+data.message+'</div></div>';
 
     $('#commits').prepend(h);
     $('commit-'+data.id).show('fast');
