1ab59f1513df6373c7b70f1d966c1dfeef2b1295
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-20 18:14:03 -0400

    New item template

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,10 @@ var socket = io.connect('http://musichackathon-commits.ex.fm');
 
 socket.on('commit', function (data) {
     console.log('Got commit!', data);
-    var h = '<div class="commit" id="commit-'+data.id+'"><div class="image"><img src="'+data.image+'"/></div><div class="header">';
-    h += '<a href="https://github.com/'+data.username+'">'+data.username+'</a>';
-    h += '<span>pushed to</span><a href="/repo/'+data.repoName+'">'+data.repoName+'</a>';
-    h += '</div><div class="message">'+data.message+'</div></div>';
+    var h = '<div class="commit row" id="commit-'+data.id+'"><div class="image span2"><img src="'+data.image+'"/></div><div class="span10"><div class="header">';
+    h += data.username;
+    h += '<span>&nbsp;pushed to&nbsp;</span>'+data.repoName;
+    h += '</div><div class="message">'+data.message+'</div></div></div>';
 
     $('#commits').prepend(h);
     $('#commit-'+data.id).show('fast');
